2950653a485d4cf356b38b4588875b3ed9f4eca0
Saif Modan <saifmodan000@gmail.com>
2026-01-20 01:47:12 -0800

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,5 @@
-from datetime import datetime, timedelta
-import secrets
-from flask import request
-from flask_restful import Resource
+# models.py
 from flask_sqlalchemy import SQLAlchemy
-# import requests
 
 db = SQLAlchemy()
 
@@ -12,16 +8,14 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    profile_image = db.Column(db.String(255))
-
+    profile_image = db.Column(db.String(255), nullable=True)
 
     reset_token = db.Column(db.String(100), nullable=True)
     token_expiry = db.Column(db.DateTime, nullable=True)
-    # tasks = db.relationship('Task', backref='user', lazy=True)
 
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    description = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
