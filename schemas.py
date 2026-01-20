5906e518584095377be86bb25f997d3477ce1034
Saif Modan <saifmodan000@gmail.com>
2026-01-20 01:46:03 -0800

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,19 +1,17 @@
+# schemas.py
 from marshmallow import Schema, fields, validate
 
 class RegisterSchema(Schema):
     username = fields.Str(required=True, validate=validate.Length(min=3))
     email = fields.Email(required=True)
     password = fields.Str(required=True, validate=validate.Length(min=6))
+
 class LoginSchema(Schema):
     username = fields.Str(required=True)
     password = fields.Str(required=True)
 
-class ForgotPasswordSchema(Schema):
+class ForgotSchema(Schema):
     email = fields.Email(required=True)
 
-class ResetPasswordSchema(Schema):
-    password = fields.Str(
-        required=True,
-        validate=validate.Length(min=6)
-    )
-
+class ResetSchema(Schema):
+    password = fields.Str(required=True, validate=validate.Length(min=6))
